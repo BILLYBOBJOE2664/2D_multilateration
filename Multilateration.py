3eb479ce59a588cc77bdf47e1830d38b30452d12
Vishan Jayasinghearachchi <vishandanura@hotmail.com>
2017-09-21 16:49:12 +0530

    Latitude/ longitude calculation added.

--- Multilateration.py
+++ Multilateration.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 
+import math
+import numpy as np
+
 # This script uses the equations described here:
 # https://en.wikipedia.org/wiki/Multilateration#math_4
+# https://stackoverflow.com/a/2188606
 
 # Get Receival times at the 3 mics A, B, C
 mic_a = input("Enter Receival time at A: ")
@@ -17,10 +21,21 @@ vector_a = [0, 0, mic_a]
 vector_b = [30, 30, mic_b]
 vector_c = [-30, 30, mic_c]
 
+#-------------------------Constants---------------------------------------------
 # Speed of sound
 # TODO: Have a separate function to calculate this. It should consider the
 # temperature and the humidity of air
+# value in metres per second (m/s)
 SOUND_VELOCITY = 340.0
+
+# value in metres (m)
+ONE_DEG_LAT_EQUATOR = 110540
+
+# value in metres (m)
+ONE_DEG_LONG_EQUATOR = 111320
+
+# LAT/ LONG of Kirigalpotta trailhead - Horton Plains NP
+origin_lat_long_arr = [6.801746, 80.806442]
 
 #-------------------------Function Definitions----------------------------------
 
@@ -46,6 +61,15 @@ def calculate_COF_D(node_arr, origin_arr):
     D = (SOUND_VELOCITY*node_arr[2] - SOUND_VELOCITY*origin_arr[2]) - ((node_arr[0]**2 + node_arr[1]**2 + 0)/SOUND_VELOCITY*node_arr[2]) + ((origin_arr[0]**2 + origin_arr[1]**2 + 0)/SOUND_VELOCITY*origin_arr[2])
     return D
 
+def find_sound_lat_long(origin_lat_long_arr, x_sound_src, y_sound_src):
+    delta_lat = y_sound_src/ONE_DEG_LAT_EQUATOR
+    delta_long = x_sound_src/(ONE_DEG_LONG_EQUATOR * math.cos(math.radians(origin_lat_long_arr[1])))
+
+    src_lat  = origin_lat_long_arr[0] + delta_lat
+    src_long = origin_lat_long_arr[1] + delta_long
+
+    sound_src_pos_lat_long = [src_lat, src_long]
+    return sound_src_pos_lat_long
 
 #-------------------------Main Program------------------------------------------
 
@@ -65,15 +89,15 @@ cof_b2 = calculate_COF_B(vector_c, vector_a)
 cof_c2 = calculate_COF_C(vector_c, vector_a)
 cof_d2 = calculate_COF_D(vector_c, vector_a)
 
-print cof_a1
-print cof_b1
-print cof_c1
-print cof_d1
-
-print cof_a2
-print cof_b2
-print cof_c2
-print cof_d2
+# print cof_a1
+# print cof_b1
+# print cof_c1
+# print cof_d1
+#
+# print cof_a2
+# print cof_b2
+# print cof_c2
+# print cof_d2
 
 # By simplifying the equations xA1 + yB1 + D1 = 0 & xA2 + yB2 + D2 = 0
 # It can be found that,
@@ -89,5 +113,18 @@ x_src = ((cof_b1/cof_b2)*cof_d2 - cof_d1)/(cof_a1 - ((cof_b1*cof_a2)/cof_b2))
 
 y_src = -1*(cof_a2*x_src + cof_d2)/cof_b2
 
+angle_to_x_axis = math.degrees(math.atan(y_src/x_src))
+
+sound_src_pos = find_sound_lat_long(origin_lat_long_arr, x_src, y_src)
+
+# alt1 = np.array([[cof_a1,cof_b1],[cof_a2,cof_b2]])
+# alt2 = np.array([-cof_d1, -cof_d2])
+# altans = np.linalg.solve(alt1,alt2)
+
 print "x_src: ", x_src
 print "y_src: ", y_src
+
+print "altans: ", altans
+
+print "Angle to X axis: ", angle_to_x_axis
+print str(sound_src_pos).strip('[]')
